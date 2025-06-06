367583a588db5876c42b3635b9eb2f71b4229854
Otto Sula <ottojanissula@gmail.com>
2025-06-06 16:37:48 +0300

    added validation and made to keep old value

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -15,14 +15,14 @@ class BookController extends Controller
     public function create() {
         return view('books.create');
     }
-
+        
     public function store(Request $request) {
-        $book = Book::create([
-            'title' => $request['title'],
-            'author' => $request['author'],
-            'released_at' => $request['released_at'],
+        $val=$request->validate([
+            'title' => 'required|unique:books|max:255',
+            'author' => 'required|max:255',
+            'released_at' => 'required',
         ]);
-
+        $book = Book::create($val);
         return redirect('/books/' . $book->id);
     }
 
@@ -38,12 +38,12 @@ class BookController extends Controller
 
     public function update(Request $request, $id) {
         $book = Book::find($id);
-        $book->update([
-            'title' => $request['title'],
-            'author' => $request['author'],
-            'released_at' => $request['released_at'],
+        $val=$request->validate([
+            'title' => 'required|unique:books|max:255',
+            'author' => 'required|max:255',
+            'released_at' => 'required',
         ]);
-
+        $book->update($val);
         return redirect('/books/' . $book->id);
     }
     public function destroy($id) {
